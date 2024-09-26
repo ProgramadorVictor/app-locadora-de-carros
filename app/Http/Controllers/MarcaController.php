0ab9d5009104e170ba994fc68a9198f0bc9e18d3
ProgramadorVictor <victorochaandrade@gmail.com>
2024-09-26 14:31:42 -0300

    !new: solicitações http com postman, usando helper tap para salvamento em database

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -19,7 +19,15 @@ class MarcaController extends Controller
 
     public function store(Request $request)
     {
-        //
+        /**
+         * As requisições estão sendo realizadas via Postman é um aplicativo feito para ver o funcionamento da API Rest
+         */
+        $marca = tap(new Marca(), function(Marca $marca) use ($request) {
+            $marca->nome = $request->input('nome');
+            $marca->imagem = $request->input('imagem');
+            $marca->save();
+        });
+        return $marca; //Laravel entende que o objeto retornado deve ser 'application/json' mesmo a gente não convertendo ele manualmente.
     }
 
     public function show(Marca $marca)
