3dc91a86676720f1f0c21b2178fecace6349c9b3
ProgramadorVictor <victorochaandrade@gmail.com>
2024-09-26 16:41:48 -0300

    !fix: response com status code, retorno mais semantico, fix problema de logica update()

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Marca;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use Illuminate\Http\JsonResponse;
 
 class MarcaController extends Controller
 {
@@ -14,10 +15,10 @@ class MarcaController extends Controller
          * Retornandos todos os registros de marca e retornando uma collection.
          * Fazendo requisição com verbo GET, pois é o verbo da index.
          */
-        return Marca::all();
+        return Marca::all(); //HTTP 200
     }
 
-    public function store(Request $request): Marca
+    public function store(Request $request): JsonResponse
     {
         /**
          * As requisições estão sendo realizadas via Postman é um aplicativo feito para ver o funcionamento da API Rest
@@ -27,7 +28,7 @@ class MarcaController extends Controller
             $marca->imagem = $request->input('imagem');
             $marca->save();
         });
-        return $marca; //Laravel entende que o objeto retornado deve ser 'application/json' mesmo a gente não convertendo ele manualmente.
+        return response()->json($marca,201); //Laravel entende que o objeto retornado deve ser 'application/json' mesmo a gente não convertendo ele manualmente.
     }
 
     // public function show(Marca $marca): Marca //Com essa tipagem retornar um Throw Exception
@@ -38,9 +39,13 @@ class MarcaController extends Controller
          */
         $marca = Marca::find($id);
         if($marca === null){ // '===' Identico precisa ser do mesmo tipo e do mesmo valor
-            return ['error' => 'O recurso nao foi encontrado']; //Retornando o array que vai ser convertido em json posteriormente pelo Laravel
+            /**
+             * Retornando uma resposta com response, indicando que a resposta é um objeto json.
+             * Códigos HTTP: oferecem mais semantica para a resposta HTTP, consulte o site: https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+             */
+            return response()->json(['error' => 'O recurso nao foi encontrado'], 404); //Passando o status para ter semântica com o retorno da resposta no lado do cliente. Anteriormente estava retornando 200.
         }
-        return $marca;
+        return response()->json($marca, 200);
     }
 
     // public function update(Request $request, Marca $marca): Marca
@@ -51,29 +56,25 @@ class MarcaController extends Controller
          * Put sendo utilizado para atualizar mais de 1 dado.
          * Patch sendo utilizado apenas para atualizar 1 dado. (soft)
          */
-        if(Marca::find($id) === null){
-            $marca = Marca::find($id);
-            $marca = tap($marca)->update([
-                'nome' => $request->input('nome'),
-                'imagem' => $request->input('imagem')
-            ]);
-            return $marca;
+        $marca = Marca::find($id);
+        if($marca === null){
+            return response()->json(['error' => 'O recurso nao foi encontrado'], 404); //Retornando o array que vai ser convertido em json posteriormente pelo Laravel
         }
-        return ['error' => 'O recurso nao foi encontrado']; //Retornando o array que vai ser convertido em json posteriormente pelo Laravel
+        $marca = tap($marca)->update([
+            'nome' => $request->input('nome'),
+            'imagem' => $request->input('imagem')
+        ]);
+        return response()->json($marca, 200);
     }
 
     public function destroy($id)
     {
         $marca = Marca::find($id);
         if($marca === null){
-            return [
-                'error' => 'O recurso nao foi encontrado!'
-            ];
+            return response()->json(['error' => 'O recurso nao foi encontrado!'], 404);
         }
         $marca->delete();
-        return [
-            'mensagem' => 'A marca foi removida com sucesso!'
-        ];
+        return response()->json(['mensagem' => 'A marca foi removida com sucesso!'], 200);
     }
     /**
      * Desta forma implementamos um web service, API OBSERVE REST fazendo operações CRUD no Model Marca.
