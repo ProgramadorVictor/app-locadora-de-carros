596959573b271cb0d2984aac8f7ba52cce6ce6e9
ProgramadorVictor <victorochaandrade@gmail.com>
2024-09-29 20:56:15 -0300

    !new: aplicando condições e filtrando o retorno da resposta json

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -8,8 +8,48 @@ use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
-    public function index()
-    {
+    public function index(Request $request)
+    {//Aidiconando filtros na consultas dos modelos e manipulando a resposta json
+        $atributos = $request->get('atributos') ? $request->get('atributos') : false; //Eu sei que esta definido abaixo, dentro do if.
+        $atributos_marca = $request->get('atributos_marca') ? $request->get('atributos_marca') : false; //Eu sei que esta definido abaixo, dentro do if.
+        //http://127.0.0.1:8000/api/modelo?atributos=id,nome,imagem,marca_id&atributos_marca=nome&filtro=nome:=:%M% //Vai trazer todos os regitros que tem 'M', nao importa o lado.
+        //Podemos fazer mais e mais comparações: http://127.0.0.1:8000/api/modelo?atributos=id,nome,imagem,marca_id&atributos_marca=nome&filtro=nome:=:%M%;abs:>:2
+        if($request->has('atributos') && $request->has('atributos_marca') && $request->has('filtro')){ ///Filtrando os damos mais ainda usando o valor filtr:=:Carro, para podemos fazer um explode de ':'   
+
+            $filtros = explode(';', $request->get('filtro')); //Separando os inumeros filtros. A query é essa http://127.0.0.1:8000/api/modelo?atributos=id,nome,imagem,marca_id&atributos_marca=nome&filtro=nome:=:%M%;abs:>:2
+            $condicional = []; $i = 0;
+            $query = Modelo::selectRaw($atributos)
+                            ->with('marca:id,'.$atributos_marca);
+
+            foreach($filtros as $filtro){//Encadeando os filtros com foreach
+                $condicional[$i] = explode(':', $filtro); //Explode
+                // var_dump($condicional[$i][0], $condicional[$i][1], $condicional[$i][2]);
+                $query->where($condicional[$i][0], $condicional[$i][1], $condicional[$i][2]);  //Respectivamente: atributo, operador, valor.
+                $i++;
+            } //Tem uma função chamada array_combine() interessante, eu deixei de usar no caso de passar mais filtros.
+            return response()->json($query->get(), 200);
+        }
+        if($request->has('atributos') && $request->has('atributos_marca')){
+            $atributos = $request->get('atributos'); //http://127.0.0.1:8000/api/modelo?atributos=id,nome,imagem
+            $atributos_marca = $request->get('atributos_marca'); //Filtrando os dados entre do relacionamento na tabela relacionada. http://127.0.0.1:8000/api/modelo?atributos=id,nome,imagem,marca_id&atributos_marca=nome,imagem
+            //Para isso modificamos o with('tabela_relacionada:parametros'); OBS: PRECISAMOS COLOCA O ID DA TABELA 'marca:id'
+            /**
+             * Utilizando com select(); da forma que o $request->get('atributos'); traz pode dar problema
+             * Por isso podemos usar o selectRaw(), ele reconhece as colunas com ',' que é o retorno de atributos do request http://127.0.0.1:8000/api/modelo?atributos=id,nome,imagem
+             */
+            // dd($atributos_marca);
+            return response()->json(Modelo::selectRaw($atributos)->with('marca:id,'.$atributos_marca)->get(),200);
+            /**
+             * Para recuperarmos o relacionamento entre as tabelas, precisamos passar a foreign do relacionamento estando na query string:
+             * http://127.0.0.1:8000/api/modelo?atributos=id,nome,imagem,marca_id
+             */
+            
+            // http://127.0.0.1:8000/api/modelo?atributos=id,nome,imagem,marca_id&atributos_marca=nome,imagem
+        }
+        if($request->has('atributos')){
+            return response()->json(Modelo::selectRaw($atributos)->get(), 200);
+        }//Se atributos_marca sozinho estiver preenchido vai trazer todos os dados abaixo.
+
         return response()->json(Modelo::with('marca')->get(), 200);
         /**
          * all() = traz uma collection.
