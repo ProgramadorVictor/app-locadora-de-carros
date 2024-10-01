3c6d3b56b8f56450ec6bb4ee9da67ead9ba4fdaf
ProgramadorVictor <victorochaandrade@gmail.com>
2024-09-30 22:19:02 -0300

    !new: implementando api webservices para os outros controllers resource

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -3,14 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Models\Carro;
-use App\Http\Requests\StoreCarroRequest;
-use App\Http\Requests\UpdateCarroRequest;
+use Illuminate\Http\Request;
+use App\Repositories\CarroRepository;
 
 class CarroController extends Controller
 {
-    public function index()
+    protected $carro;
+    public function __construct(Carro $carro) {
+        $this->carro = $carro;
+    }
+
+    public function index(Request $request)
     {
-        //
+        $carroRepository = new CarroRepository($this->carro);
+
+        if($request->has('atributos_modelo')) {
+            $atributos_modelo = 'modelo:id,'.$request->atributos_modelo;
+            $carroRepository->selectAtributosRelacionados($atributos_modelo);
+        } else {
+            $carroRepository->selectAtributosRelacionados('modelo');
+        }
+
+        if($request->has('filtro')) {
+            $carroRepository->filtro($request->filtro);
+        }
+
+        if($request->has('atributos')) {
+            $carroRepository->selectAtributos($request->atributos);
+        } 
+
+        return response()->json($carroRepository->getResultado(), 200);
     }
 
     public function create()
@@ -18,28 +40,78 @@ class CarroController extends Controller
         //
     }
 
-    public function store(StoreCarroRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->carro->rules());
+
+        $carro = $this->carro->create([
+            'modelo_id' => $request->modelo_id,
+            'placa' => $request->placa,
+            'disponivel' => $request->disponivel,
+            'km' => $request->km
+        ]);
+
+        return response()->json($carro, 201);
     }
 
-    public function show(Carro $carro)
+    public function show($id)
     {
-        //
+        $carro = $this->carro->with('modelo')->find($id);
+        if($carro === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404) ;
+        } 
+
+        return response()->json($carro, 200);
     }
 
     public function edit(Carro $carro)
     {
         //
     }
-
-    public function update(UpdateCarroRequest $request, Carro $carro)
+    
+    public function update(Request $request, $id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if($carro === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
+        }
+
+        if($request->method() === 'PATCH') {
+
+            $regrasDinamicas = array();
+
+            //percorrendo todas as regras definidas no Model
+            foreach($carro->rules() as $input => $regra) {
+                
+                //coletar apenas as regras aplicáveis aos parâmetros parciais da requisição PATCH
+                if(array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            
+            $request->validate($regrasDinamicas);
+
+        } else {
+            $request->validate($carro->rules());
+        }
+        
+        $carro->fill($request->all());
+        $carro->save();
+        
+        return response()->json($carro, 200);
     }
-
-    public function destroy(Carro $carro)
+    
+    public function destroy($id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if($carro === null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe'], 404);
+        }
+
+        $carro->delete();
+        return response()->json(['msg' => 'O carro foi removido com sucesso!'], 200);
+        
     }
 }
--- app/Repositories/ModeloRepository.php
+++ app/Repositories/ModeloRepository.php
@@ -2,10 +2,6 @@
 
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model; //Use se tornou absoleto apos o extends
-
-class ModeloRepository extends AbstractRepository{
-        /**
-     * Caso tenha um método próprio regras da propria classe poderiamos colocar aqui
-     */
+class ModeloRepository extends AbstractRepository {
+    
 }
