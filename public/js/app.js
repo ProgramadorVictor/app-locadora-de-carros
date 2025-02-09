a38e7f7142b04f47678c0557bd40c55585d4fda1
ProgramadorVictor <victorochaandrade@gmail.com>
2025-02-09 19:24:54 -0300

    implementando autenticação com web session e jwt token e adicionando token jwt aos cookies no front-end

--- public/js/app.js
+++ public/js/app.js
@@ -2180,7 +2180,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   props: {
@@ -2200,6 +2199,34 @@ __webpack_require__.r(__webpack_exports__);
     csrf_token: {
       type: String,
       required: true
+    }
+  },
+  data: function data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    login: function login(e) {
+      var url = 'http://127.0.0.1:8000/api/login';
+      var configs = {
+        method: 'post',
+        body: new URLSearchParams({
+          email: this.email,
+          password: this.password
+        })
+      };
+      fetch(url, configs).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        if (data.token) {
+          document.cookie = 'token=' + data.token + ';SameSite=Lax';
+          //Automaticamente o laravel reconhece o 'token=' entender que o token é de autorização. Nativamente ele vai ser reconhecido pelo Laravel
+        }
+        e.target.submit(); //Colocando aqui dentro pois espera a conclusão dos then para disparar o evento.
+      });
+      // e.target.submit(); //Disparando o evento, pode ocorrer problema pois, o fetch é assincrono
     }
   },
   // props: ['nome','uid', 'idade'], //Uma forma mais simples sem a validação.
@@ -37657,47 +37684,139 @@ var render = function () {
           _c("div", { staticClass: "card-header" }, [_vm._v("Login")]),
           _vm._v(" "),
           _c("div", { staticClass: "card-body" }, [
-            _c("form", { attrs: { method: "POST", action: "" } }, [
-              _c("div", { staticClass: "form-group row" }, [
-                _c(
-                  "label",
-                  {
-                    staticClass: "col-md-4 col-form-label text-md-right",
-                    attrs: { for: "email" },
+            _c(
+              "form",
+              {
+                attrs: { method: "POST", action: "" },
+                on: {
+                  submit: function ($event) {
+                    $event.preventDefault()
+                    return _vm.login($event)
                   },
-                  [_vm._v("Props do Vue")]
-                ),
-                _vm._v(" "),
-                _c("div", { staticClass: "col-md-6" }, [
-                  _c("input", {
-                    staticClass: "form-control",
-                    attrs: {
-                      id: "props",
-                      type: "text",
-                      name: "props",
-                      disabled: "",
-                      autocomplete: "off",
+                },
+              },
+              [
+                _c("div", { staticClass: "form-group row" }, [
+                  _c(
+                    "label",
+                    {
+                      staticClass: "col-md-4 col-form-label text-md-right",
+                      attrs: { for: "email" },
                     },
-                    domProps: { value: _vm.kebabCase + ", " + _vm.abcde },
-                  }),
+                    [_vm._v("Props do Vue")]
+                  ),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "col-md-6" }, [
+                    _c("input", {
+                      staticClass: "form-control",
+                      attrs: {
+                        id: "props",
+                        type: "text",
+                        name: "props",
+                        disabled: "",
+                        autocomplete: "off",
+                      },
+                      domProps: { value: _vm.kebabCase + ", " + _vm.abcde },
+                    }),
+                  ]),
                 ]),
-              ]),
-              _vm._v(" "),
-              _c("input", {
-                attrs: { type: "hidden", name: "_token" },
-                domProps: { value: _vm.csrf_token },
-              }),
-              _vm._v(" "),
-              _c("div", { staticClass: "text-center" }),
-              _vm._v(" "),
-              _vm._m(0),
-              _vm._v(" "),
-              _vm._m(1),
-              _vm._v(" "),
-              _vm._m(2),
-              _vm._v(" "),
-              _vm._m(3),
-            ]),
+                _vm._v(" "),
+                _c("input", {
+                  attrs: { type: "hidden", name: "_token" },
+                  domProps: { value: _vm.csrf_token },
+                }),
+                _vm._v(" "),
+                _c("div", { staticClass: "text-center" }),
+                _vm._v(" "),
+                _c("div", { staticClass: "form-group row" }, [
+                  _c(
+                    "label",
+                    {
+                      staticClass: "col-md-4 col-form-label text-md-right",
+                      attrs: { for: "email" },
+                    },
+                    [_vm._v("Endereço de e-mail")]
+                  ),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "col-md-6" }, [
+                    _c("input", {
+                      directives: [
+                        {
+                          name: "model",
+                          rawName: "v-model",
+                          value: _vm.email,
+                          expression: "email",
+                        },
+                      ],
+                      staticClass: "form-control",
+                      attrs: {
+                        id: "email",
+                        type: "email",
+                        name: "email",
+                        value: "",
+                        required: "",
+                        autocomplete: "email",
+                        autofocus: "",
+                      },
+                      domProps: { value: _vm.email },
+                      on: {
+                        input: function ($event) {
+                          if ($event.target.composing) {
+                            return
+                          }
+                          _vm.email = $event.target.value
+                        },
+                      },
+                    }),
+                  ]),
+                ]),
+                _vm._v(" "),
+                _c("div", { staticClass: "form-group row" }, [
+                  _c(
+                    "label",
+                    {
+                      staticClass: "col-md-4 col-form-label text-md-right",
+                      attrs: { for: "password" },
+                    },
+                    [_vm._v("Senha")]
+                  ),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "col-md-6" }, [
+                    _c("input", {
+                      directives: [
+                        {
+                          name: "model",
+                          rawName: "v-model",
+                          value: _vm.password,
+                          expression: "password",
+                        },
+                      ],
+                      staticClass: "form-control",
+                      attrs: {
+                        id: "password",
+                        type: "password",
+                        name: "password",
+                        required: "",
+                        autocomplete: "current-password",
+                      },
+                      domProps: { value: _vm.password },
+                      on: {
+                        input: function ($event) {
+                          if ($event.target.composing) {
+                            return
+                          }
+                          _vm.password = $event.target.value
+                        },
+                      },
+                    }),
+                  ]),
+                ]),
+                _vm._v(" "),
+                _vm._m(0),
+                _vm._v(" "),
+                _vm._m(1),
+              ]
+            ),
           ]),
         ]),
       ]),
@@ -37705,64 +37824,6 @@ var render = function () {
   ])
 }
 var staticRenderFns = [
-  function () {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "form-group row" }, [
-      _c(
-        "label",
-        {
-          staticClass: "col-md-4 col-form-label text-md-right",
-          attrs: { for: "email" },
-        },
-        [_vm._v("Endereço de e-mail")]
-      ),
-      _vm._v(" "),
-      _c("div", { staticClass: "col-md-6" }, [
-        _c("input", {
-          staticClass: "form-control",
-          attrs: {
-            id: "email",
-            type: "email",
-            name: "email",
-            value: "",
-            required: "",
-            autocomplete: "email",
-            autofocus: "",
-          },
-        }),
-      ]),
-    ])
-  },
-  function () {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "form-group row" }, [
-      _c(
-        "label",
-        {
-          staticClass: "col-md-4 col-form-label text-md-right",
-          attrs: { for: "password" },
-        },
-        [_vm._v("Senha")]
-      ),
-      _vm._v(" "),
-      _c("div", { staticClass: "col-md-6" }, [
-        _c("input", {
-          staticClass: "form-control",
-          attrs: {
-            id: "password",
-            type: "password",
-            name: "password",
-            required: "",
-            autocomplete: "current-password",
-          },
-        }),
-      ]),
-    ])
-  },
   function () {
     var _vm = this
     var _h = _vm.$createElement
--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -6,8 +6,7 @@
                 <div class="card">
                     <div class="card-header">Login</div>
                     <div class="card-body">
-                        <form method="POST" action="">
-                            
+                        <form method="POST" action="" @submit.prevent="login($event)">
                             <div class="form-group row">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">Props do Vue</label>
 
@@ -27,7 +26,7 @@
                                 <label for="email" class="col-md-4 col-form-label text-md-right">Endereço de e-mail</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus>
+                                    <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus v-model="email">
 
                                 </div>
                             </div>
@@ -36,7 +35,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password" v-model="password">
                                 </div>
                             </div>
 
@@ -93,6 +92,35 @@
                 type: String,
                 required: true
             },
+        },
+        data(){
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            login(e){
+                let url = 'http://127.0.0.1:8000/api/login'
+                let configs = {
+                    method: 'post',
+                    body: new URLSearchParams({
+                        email: this.email,
+                        password: this.password
+                    })
+                }
+                fetch(url, configs)
+                    .then(response => response.json())
+                    .then(data => {
+                        
+                        if(data.token){
+                            document.cookie = 'token=' + data.token +';SameSite=Lax'
+                            //Automaticamente o laravel reconhece o 'token=' entender que o token é de autorização. Nativamente ele vai ser reconhecido pelo Laravel
+                        }
+                        e.target.submit(); //Colocando aqui dentro pois espera a conclusão dos then para disparar o evento.
+                    })
+                // e.target.submit(); //Disparando o evento, pode ocorrer problema pois, o fetch é assincrono
+            }
         },
         // props: ['nome','uid', 'idade'], //Uma forma mais simples sem a validação.
         mounted() {
